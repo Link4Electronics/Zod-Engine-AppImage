f7f72e36b17d7263ec1636b10410ddcb09acb592
Link4Electronics <link4electronics@gmail.com>
2026-03-17 05:16:35 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,14 +6,21 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    libdecor     \
+    sdl12-compat \
+    sdl_image    \
+    sdl_mixer    \
+    sdl_ttf
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package gtk2
+make-aur-package wxgtk2.8
+make-aur-package libmysqlclient
 
 # If the application needs to be manually built that has to be done down here
 wget https://master.dl.sourceforge.net/project/zod/linux_releases/zod_linux-2011-09-06.tar.gz
